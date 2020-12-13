cb89e83f7f1c01c40f8e062187f7af9281850b8e
Matheus Thomaz <matheustthomaz@gmail.com>
2020-12-13 05:57:29 -0300

    Dashboard page styles added.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>
-}
+  return (
+    <>
+      <img src={logoImg} alt="logo" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+          src="https://avatars2.githubusercontent.com/u/60274043?s=460&v=4"
+          alt="profile"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+          src="https://avatars2.githubusercontent.com/u/60274043?s=460&v=4"
+          alt="profile"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+          src="https://avatars2.githubusercontent.com/u/60274043?s=460&v=4"
+          alt="profile"
+          />
+          <div>
+            <strong>rocketseat unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
